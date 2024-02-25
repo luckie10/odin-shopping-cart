d82e439aa4d3190e9612de8ead86887a6ce89358
Boban Kilvin <btrajcevski10@gmail.com>
2024-02-25 00:47:31 -0500

    add product card

---
+++ src/features/store/components/ProductCard.jsx
@@ -0,0 +1,30 @@
+import styles from "./ProductCard.module.css";
+import { Icon } from "@mdi/react";
+import { mdiCartVariant, mdiPlus, mdiMinus } from "@mdi/js";
+
+export const ProductCard = ({ product }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <img
+        className={styles.productImage}
+        src={product.image}
+        alt={product.title}
+      />
+      <h1 className={styles.productName}>{product.title}</h1>
+      <div className={styles.cartButtonsContainer}>
+        <div className={styles.quantityButtonsContainer}>
+          <button className={styles.cartButton}>
+            <Icon path={mdiMinus} size={1} />
+          </button>
+          <input type="text" value="0" className={styles.quantityInput} />
+          <button className={styles.cartButton}>
+            <Icon path={mdiPlus} size={1} />
+          </button>
+        </div>
+        <button className={styles.addCartButton}>
+          <Icon path={mdiCartVariant} size={1} />
+        </button>
+      </div>
+    </div>
+  );
+};
