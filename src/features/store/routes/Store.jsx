ba858bc739aa48f2bbab2e18e0af9f6ca1136a77
Boban Kilvin <btrajcevski10@gmail.com>
2024-02-25 00:48:02 -0500

    add proucts to store page

--- src/features/store/routes/Store.jsx
+++ src/features/store/routes/Store.jsx
@@ -1,3 +1,19 @@
+import { useProducts } from "../api/products";
+import { ProductCard } from "../components/ProductCard";
+
+import styles from "./Store.module.css";
+
 export const Store = () => {
-  return <>I am the Store page</>;
+  const { products, error, loading } = useProducts();
+
+  if (loading) return "Loading...";
+  if (error) console.error(error);
+
+  return (
+    <div className={styles.storeContainer}>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
