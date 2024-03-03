80d09276a2b7f8569c688e550f91a9b2605a7fee
Boban Kilvin <btrajcevski10@gmail.com>
2024-03-03 03:09:02 -0500

    add quantity function

--- src/features/store/components/ProductCard.jsx
+++ src/features/store/components/ProductCard.jsx
@@ -3,7 +3,26 @@ import { Icon } from "@mdi/react";
 import { mdiCartVariant, mdiPlus, mdiMinus } from "@mdi/js";
 
 import styles from "./ProductCard.module.css";
+
 export const ProductCard = ({ product }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const onInputChange = (setStateFunction) => {
+    return (event) => {
+      setStateFunction(event.target.value);
+    };
+  };
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity === 0) return;
+
+    setQuantity(quantity - 1);
+  };
+
   return (
     <div className={styles.cardContainer}>
       <img
@@ -14,11 +33,17 @@ export const ProductCard = ({ product }) => {
       <h1 className={styles.productName}>{product.title}</h1>
       <div className={styles.cartButtonsContainer}>
         <div className={styles.quantityButtonsContainer}>
-          <button className={styles.cartButton}>
+          <button className={styles.cartButton} onClick={decrementQuantity}>
             <Icon path={mdiMinus} size={1} />
           </button>
-          <input type="text" value="0" className={styles.quantityInput} />
-          <button className={styles.cartButton}>
+          <input
+            type="text"
+            inputMode="numeric"
+            className={styles.quantityInput}
+            value={quantity}
+            onChange={onInputChange(setQuantity)}
+          />
+          <button className={styles.cartButton} onClick={incrementQuantity}>
             <Icon path={mdiPlus} size={1} />
           </button>
         </div>
