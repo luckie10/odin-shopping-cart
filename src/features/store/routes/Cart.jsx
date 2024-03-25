08caac0e4892bebb5b04f78985a0651cd59d3c60
Boban Kilvin <btrajcevski10@gmail.com>
2024-03-25 04:22:57 -0400

    add cart page

--- src/features/store/routes/Cart.jsx
+++ src/features/store/routes/Cart.jsx
@@ -1,3 +1,55 @@
+import { useCart } from "@/providers/CartProvider/CartContext";
+import { useProductsContext } from "@/providers/ProductProvider/ProductContext";
+
+import styles from "./Cart.module.css";
+
 export const Cart = () => {
-  return <>I am the Cart page </>;
+  const cart = useCart();
+  const { products, error, loading } = useProductsContext();
+
+  const getProductById = (products, id) =>
+    products.filter((product) => product.id === id)[0];
+
+  const composeCartItem = (item) => {
+    const product = getProductById(products, item.id);
+
+    return (
+      <div className={styles.itemContainer}>
+        <img
+          className={styles.productImage}
+          src={product.image}
+          alt={product.title}
+        />
+        <div>
+          <h1 className={styles.productName}>{product.title}</h1>
+          <div className="quantity">{item.quantity}</div>
+        </div>
+        <span className="price">${product.price}</span>
+      </div>
+    );
+  };
+
+  const calculateSubtotal = () => {
+    const total = cart.reduce((accumulator, item) => {
+      const product = getProductById(products, item.id);
+      const price = product.price;
+      return accumulator + price * item.quantity;
+    }, 0);
+
+    return Math.round((total + Number.EPSILON) * 100) / 100;
+  };
+
+  if (loading) return "Loading...";
+  if (error) console.error(error);
+
+  return (
+    <div className={styles.cartContainer}>
+      <div className={styles.itemsContainer}>
+        {cart.map((item) => composeCartItem(item))}
+      </div>
+      <div className={styles.checkoutContainer}>
+        Subtotal: {calculateSubtotal()}
+      </div>
+    </div>
+  );
 };
